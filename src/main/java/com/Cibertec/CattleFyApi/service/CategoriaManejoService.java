5171d2380243a928c2b4205f5a3e52d94be55212
Vector404 <victornarazas2005@gmail.com>
2025-12-05 18:27:29 -0500

    Solucionand error

--- src/main/java/com/Cibertec/CattleFyApi/service/CategoriaManejoService.java
+++ src/main/java/com/Cibertec/CattleFyApi/service/CategoriaManejoService.java
@@ -3,9 +3,11 @@ package com.Cibertec.CattleFyApi.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.Cibertec.CattleFyApi.repository.ICategoriaManejoRepository;
+
 @Service
 public class CategoriaManejoService {
 
     @Autowired
-    CategoriaManejoService categoriaManejoService;
+    ICategoriaManejoRepository categoriaManejoRepository;
 }
