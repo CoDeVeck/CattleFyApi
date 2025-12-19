8f11d72398e73e5a98232f0f1f156e8c5c8c8ea6
Vector404 <victornarazas2005@gmail.com>
2025-12-18 21:03:25 -0500

    Insertando datos de pruebas para los registros

---
+++ src/main/java/com/Cibertec/CattleFyApi/dto/ReporteGrafico1.java
@@ -0,0 +1,8 @@
+package com.Cibertec.CattleFyApi.dto;
+
+import java.time.LocalDate;
+
+public class ReporteGrafico1 {
+    private LocalDate  fecha;
+    private double peso_kg;
+}
---
+++ src/main/java/com/Cibertec/CattleFyApi/dto/ReporteProduccionEngordeDTO.java
@@ -0,0 +1,10 @@
+package com.Cibertec.CattleFyApi.dto;
+
+import lombok.Data;
+
+@Data
+public class ReporteProduccionEngordeDTO {
+
+    private double precio_promedio;
+    private double ganancia_kg;
+}
